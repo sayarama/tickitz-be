133824b8ffca5c000b787e3c78b8dd9e516c36ee
Aulia Ramadhan <auliarama965@gmail.com>
2023-09-29 19:22:04 +0700

    Update Backend

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 3000;
 const database = require("./database");
 
+// Gran access expres json from outside
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
 app.get("/movies", async (req, res) => {
   try {
     const request = await database`SELECT id, name, genres, poster, duration FROM movies`;
@@ -15,15 +20,15 @@ app.get("/movies", async (req, res) => {
   } catch (error) {
     res.status(502).json({
       status: false,
-      message: "Something weong in our server",
+      message: "Something wrong in our server",
       data: [],
-    })
+    });
   }
 });
 
 app.get("/movies/:id", async (req, res) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
     const request = await database`SELECT * FROM movies WHERE id = ${id}`;
 
     res.json({
@@ -36,11 +41,50 @@ app.get("/movies/:id", async (req, res) => {
       status: false,
       message: "Something weong in our server",
       data: [],
-    })
+    });
   }
 });
 
+app.post("/movies", async (req, res) => {
+  try {
+    const {
+      name,
+      release_date,
+      duration,
+      genres,
+      directed_by,
+      casts,
+      synopsis,
+      poster,
+    } = req.body;
 
-app.listen(port, () => {
+    const request = await database`INSERT INTO movies
+    (name, release_date, duration, genres, directed_by, casts, synopsis, poster)
+  values
+    (${name}, ${release_date}, ${duration}, ${genres}, ${directed_by}, ${casts}, ${synopsis}, ${poster}) RETURNING id`;
+
+  if (request.length > 0) {
+    res.json({
+      status: true,
+      message: "Insert Data Success",
+    });
+  } else {
+    res.status(400).json({
+      status: false,
+      message: "Bad Input, please make sure your input is complete"
+    });
+  }
+
+    
+  } catch (error) {
+    res.status(502).json({
+      status: false,
+      message: "Something wrong in our server",
+      data: [],
+    });
+  }
+});
+
+app.listen(port, () => { 
   console.log(`Example app listening on port ${port}`);
 });
