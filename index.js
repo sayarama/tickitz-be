78fc86006d2564d1c2b7ab1c29a298374a327c46
Aulia Ramadhan <auliarama965@gmail.com>
2023-09-29 20:26:22 +0700

    Update Users Backend

--- index.js
+++ index.js
@@ -201,6 +201,99 @@ app.post("/cinemas", async (req, res) => {
 
 // START USER BACKEND
 
+app.get("/users", async (req, res) => {
+  try {
+    const request =
+      await database`SELECT id, first_name, last_name, phone_number, email FROM users`;
+
+    res.json({
+      status: true,
+      message: "Get Data Success",
+      data: request,
+    });
+  } catch (error) {
+    res.status(502).json({
+      status: false,
+      message: "Something wrong in our server",
+      data: [],
+    });
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const request = await database`SELECT * FROM users WHERE id = ${id}`;
+
+    res.json({
+      status: true,
+      message: "Get Data Success",
+      data: request,
+    });
+  } catch (error) {
+    res.status(502).json({
+      status: false,
+      message: "Something wrong in our server",
+      data: [],
+    });
+  }
+});
+
+app.post("/cinemas", async (req, res) => {
+  try {
+    const {
+      movie_id,
+      name,
+      city,
+      adress,
+      show_times,
+      price,
+      logo,
+    } = req.body;
+
+    const isInputValid =
+      movie_id &&
+      name &&
+      city &&
+      adress &&
+      show_times &&
+      price &&
+      logo;
+
+      // check if input is valid
+
+    if (!isInputValid) {
+      res.status(400).json({
+        status: false,
+        message: "Bad Input, please make sure your input is complete",
+      });
+    }
+
+    const request = await database`INSERT INTO cinemas
+    (movie_id, name, city, adress, show_times, price, logo)
+  values
+    (${movie_id}, ${name}, ${city}, ${adress}, ${show_times}, ${price}, ${logo}) RETURNING id`;
+
+    if (request.length > 0) {
+      res.json({
+        status: true,
+        message: "Insert Data Success",
+      });
+    } else {
+      res.status(400).json({
+        status: false,
+        message: "Bad Input, please make sure your input is complete",
+      });
+    }
+  } catch (error) {
+    res.status(502).json({
+      status: false,
+      message: "Something wrong in our server",
+      data: [],
+    });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
