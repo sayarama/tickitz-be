b59d768439c11c4529fbe28e17c4c97defda3f6a
Aulia Ramadhan <auliarama965@gmail.com>
2023-10-06 19:38:48 +0700

    Update CRUD Movies

--- index.js
+++ index.js
@@ -5,15 +5,41 @@ const database = require("./database");
 app.use(express.json());
 let port = process.env.PORT;
 
+// Get
 app.get("/movies", async (req, res) => {
   const request = await database`SELECT * FROM movies`;
   res.send(request);
 });
 
+// Get selected id
 app.get("/movies/:id", async (req, res) => {
-  const request = await database`SELECT * FROM movies`;
+  const id = Number(req.params.id)
+  const request = await database`SELECT * FROM movies WHERE id=${id}`;
   res.send(request);
 });
+
+// Post
+app.post("/movies", async (req, res) => {
+  const {name, release_date, duration, genres, directed_by, casts, synopsis, poster} = req.body
+  const request = await database`INSERT INTO movies(name, release_date, duration, genres, directed_by, casts, synopsis, poster) VALUES(${name},${release_date},${duration},${genres},${directed_by},${casts},${synopsis},${poster})`;
+  res.send("data created");
+});
+
+// Update
+app.put('/movies/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  const {name, release_date, duration, genres, directed_by, casts, synopsis, poster} = req.body
+  const request = await database `UPDATE movies SET name=${name}, release_date=${release_date}, duration=${duration}, genres=${genres}, directed_by=${directed_by}, casts=${casts}, synopsis=${synopsis}, poster=${poster} WHERE id=${id}`
+  res.send("data updated")
+})
+
+// Delete
+app.delete('/movies/:id', async(req, res) => {
+  const id = Number(req.params.id)
+  const request = await database`DELETE FROM movies WHERE id=${id}`;
+  res.send("data deleted")
+})
+
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
@@ -64,61 +90,61 @@ app.listen(port, () => {
 //   }
 // });
 
-// app.post("/movies", async (req, res) => {
-//   try {
-//     const {
-//       name,
-//       release_date,
-//       duration,
-//       genres,
-//       directed_by,
-//       casts,
-//       synopsis,
-//       poster,
-//     } = req.body;
+app.post("/movies", async (req, res) => {
+  try {
+    const {
+      name,
+      release_date,
+      duration,
+      genres,
+      directed_by,
+      casts,
+      synopsis,
+      poster,
+    } = req.body;
 
-//     const isInputValid =
-//       name &&
-//       release_date &&
-//       duration &&
-//       directed_by &&
-//       casts &&
-//       synopsis &&
-//       poster;
+    const isInputValid =
+      name &&
+      release_date &&
+      duration &&
+      directed_by &&
+      casts &&
+      synopsis &&
+      poster;
 
-//       // check if input is valid
+      // check if input is valid
 
-//     if (!isInputValid) {
-//       res.status(400).json({
-//         status: false,
-//         message: "Bad Input, please make sure your input is complete",
-//       });
-//     }
+    if (!isInputValid) {
+      res.status(400).json({
+        status: false,
+        message: "Bad Input, please make sure your input is complete",
+      });
+    }
 
-//     const request = await database`INSERT INTO movies
-//     (name, release_date, duration, genres, directed_by, casts, synopsis, poster)
-//   values
-//     (${name}, ${release_date}, ${duration}, ${genres}, ${directed_by}, ${casts}, ${synopsis}, ${poster}) RETURNING id`;
+    const request = await database`INSERT INTO movies
+    (name, release_date, duration, genres, directed_by, casts, synopsis, poster)
+  values
+    (${name}, ${release_date}, ${duration}, ${genres}, ${directed_by}, ${casts}, ${synopsis}, ${poster}) RETURNING id`;
 
-//     if (request.length > 0) {
-//       res.json({
-//         status: true,
-//         message: "Insert Data Success",
-//       });
-//     } else {
-//       res.status(400).json({
-//         status: false,
-//         message: "Bad Input, please make sure your input is complete",
-//       });
-//     }
-//   } catch (error) {
-//     res.status(502).json({
-//       status: false,
-//       message: "Something wrong in our server",
-//       data: [],
-//     });
-//   }
-// });
+    if (request.length > 0) {
+      res.json({
+        status: true,
+        message: "Insert Data Success",
+      });
+    } else {
+      res.status(400).json({
+        status: false,
+        message: "Bad Input, please make sure your input is complete",
+      });
+    }
+  } catch (error) {
+    res.status(502).json({
+      status: false,
+      message: "Something wrong in our server",
+      data: [],
+    });
+  }
+});
 
 // // ENDPOINT CINEMAS
 
