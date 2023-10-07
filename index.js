c6de96f99be9c20145bc13ea47a54fe3195ad31a
Aulia Ramadhan <auliarama965@gmail.com>
2023-10-07 14:55:32 +0700

    Update Movies Endpoint

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const database = require("./database");
-const cors = require("cors")
-const helmet = require("helmet")
+const cors = require("cors");
+const helmet = require("helmet");
 app.use(express.json());
 let port = process.env.PORT;
 
 let corsOptions = {
-  origin: '*',
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
-}
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
 
 app.use(cors(corsOptions));
 
@@ -18,39 +18,100 @@ app.use(helmet());
 
 // Get
 app.get("/movies", async (req, res) => {
-  const request = await database`SELECT * FROM movies`;
-  res.send(request);
+  try {
+    const request = await database`SELECT * FROM movies`;
+    res.status('200').json({
+      status: true,
+      message: "Get data success",
+      data: request,
+    })
+  } catch (error) {
+    res.status("502").json({
+      status: false,
+      message: "something wrong in our server",
+      data: [],
+    });
+  }
 });
 
 // Get selected id
 app.get("/movies/:id", async (req, res) => {
-  const id = Number(req.params.id)
-  const request = await database`SELECT * FROM movies WHERE id=${id}`;
-  res.send(request);
+  try {
+    const id = Number(req.params.id);
+    const request = await database`SELECT * FROM movies WHERE id=${id}`;
+    res.status('200').json({
+      status: true,
+      message: "Get data success",
+      data: request,
+    })
+  } catch (error) {
+    res.status("502").json({
+      status: false,
+      message: "something wrong in our server",
+      data: [],
+    });
+  }
 });
 
 // Post
 app.post("/movies", async (req, res) => {
-  const {name, release_date, duration, genres, directed_by, casts, synopsis, poster} = req.body
-  const request = await database`INSERT INTO movies(name, release_date, duration, genres, directed_by, casts, synopsis, poster) VALUES(${name},${release_date},${duration},${genres},${directed_by},${casts},${synopsis},${poster})`;
-  res.send("data created");
+  try {
+    const {
+      name,
+      release_date,
+      duration,
+      genres,
+      directed_by,
+      casts,
+      synopsis,
+      poster,
+    } = req.body;
+    const request =
+      await database`INSERT INTO movies(name, release_date, duration, genres, directed_by, casts, synopsis, poster) VALUES(${name},${release_date},${duration},${genres},${directed_by},${casts},${synopsis},${poster})`;
+      res.status("200").json({
+        status: true,
+        message: "Get data success",
+        data: request,
+      });
+  } catch (error) {
+    res.status("502").json({
+      status: false,
+      message: "something wrong in our server",
+      data: [],
+    });
+  }
 });
 
 // Update
-app.put('/movies/:id', async (req, res) => {
-  const id = Number(req.params.id)
-  const {name, release_date, duration, genres, directed_by, casts, synopsis, poster} = req.body
-  const request = await database `UPDATE movies SET name=${name}, release_date=${release_date}, duration=${duration}, genres=${genres}, directed_by=${directed_by}, casts=${casts}, synopsis=${synopsis}, poster=${poster} WHERE id=${id}`
-  res.send("data updated")
-})
+app.put("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const {
+    name,
+    release_date,
+    duration,
+    genres,
+    directed_by,
+    casts,
+    synopsis,
+    poster,
+  } = req.body;
+  const request =
+    await database`UPDATE movies SET name=${name}, release_date=${release_date}, duration=${duration}, genres=${genres}, directed_by=${directed_by}, casts=${casts}, synopsis=${synopsis}, poster=${poster} WHERE id=${id}`;
+  res.send("data updated");
+});
 
 // Delete
-app.delete('/movies/:id', async(req, res) => {
-  const id = Number(req.params.id)
+app.delete("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
   const request = await database`DELETE FROM movies WHERE id=${id}`;
-  res.send("data deleted")
-})
+  res.send("data deleted");
+});
 
+// Endpoint Users
+
+// Get users
+
+app.get("/users", async (req, res) => {});
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
