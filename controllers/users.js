33ed0914a91d64ddb3b8ec075f3e1462c4d1c66d
Aulia Ramadhan <auliarama965@gmail.com>
2023-10-09 15:18:20 +0700

    Check JWT Validation

--- controllers/users.js
+++ controllers/users.js
@@ -162,7 +162,30 @@ const usersController = {
                 data: [],
             });
         }
-    }
+    },
+    _checkJwt: async (req, res, next) => {
+        try {
+            const token = req.headers.authorization.slice(7);
+            const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
+
+            if (decoded) {
+                next();
+            } else {
+                res.status(401).json({
+                    status: false,
+                    message: "Token error",
+                    data: [],
+                })
+            }
+
+        } catch (error) {
+            res.status(401).json({
+                status: false,
+                message: "Token error",
+                data: [],
+            })
+        }
+    },
 }
 
 module.exports = usersController;
