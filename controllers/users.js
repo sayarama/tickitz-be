19af80d70647e9d7756a25ab2576e94b207e6992
Aulia Ramadhan <auliarama965@gmail.com>
2023-10-09 14:52:37 +0700

    Update Users Validation

--- controllers/users.js
+++ controllers/users.js
@@ -17,7 +17,93 @@ const usersController = {
                 data: [],
             });
         }
-    }
+    },
+    _validationAddUsers: async (req, res, next) => {
+        const schema = new Validator(req.body, {
+            first_name: "required|minLength:1|maxLength:100",
+            last_name: "required|minLength:1|maxLength:100",
+            phone_number: "required|phoneNumber",
+            email: "required|email",
+            password: "required|minLength:2",
+            photo_profile: "required|url",
+        })
+
+        schema.check().then((matched) => {
+            if (!matched) {
+                res.status(422).send({
+                    status: false,
+                    message: schema.errors,
+                    data: null
+                })
+                return;
+            } else {
+                next();
+            }
+        })
+    } ,
+    _addUsers: async (req, res) => {
+        try {
+            const {
+                first_name,
+                last_name,
+                phone_number,
+                email,
+                password,
+                photo_profile,
+            } = req.body;
+    
+            const isInputValid =
+                first_name &&
+                last_name &&
+                phone_number &&
+                email &&
+                password &&
+                photo_profile;
+    
+            if (!isInputValid) {
+                res.status(400).json({
+                    status: false,
+                    message: "Bad input, please make sure your input is completed",
+                });
+                return;
+            }
+    
+            // Check Unique Email
+            const checkEmail =
+                await usersModel.checkEmail(email);
+    
+            if (checkEmail.length > 0) {
+                res.status(400).json({
+                    status: false,
+                    message: "Email is already registered",
+                });
+    
+                return;
+            }
+            const request = await usersModel.addUsers({
+                first_name,
+                last_name,
+                phone_number,
+                email,
+                password,
+                photo_profile,
+            });
+            
+            if (request.length > 0) {
+                res.json({
+                    status: true,
+                    message: "Insert data success",
+                });
+                return;
+            }
+        } catch (error) {
+            res.status("502").json({
+                status: false,
+                message: "something wrong in our server",
+                data: [],
+            });
+        }
+    } 
 }
 
 module.exports = usersController;
--- routers/users.js
+++ routers/users.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const database = require("../database");
 const usersModel = require("../models/users");
-const usersController = require("../controllers/users")
+const usersController = require("../controllers/users");
 const router = require("express").Router();
 const { Validator } = require("node-input-validator");
 
@@ -38,89 +38,11 @@ const checkJwt = async (req, res, next) => {
 router.get("/users", usersController._getAllUsers);
 
 // User register
-router.post("/users/register", async (req, res) => {
-    try {
-        const {
-            first_name,
-            last_name,
-            phone_number,
-            email,
-            password,
-            photo_profile,
-        } = req.body;
-
-        const isInputValid =
-            first_name &&
-            last_name &&
-            phone_number &&
-            email &&
-            password &&
-            photo_profile;
-
-        if (!isInputValid) {
-            res.status(400).json({
-                status: false,
-                message: "Bad input, please make sure your input is completed",
-            });
-            return;
-        }
-
-        const schema = new Validator(req.body, {
-            first_name: "required|minLength:1|maxLength:100",
-            last_name: "required|minLength:1|maxLength:100",
-            phone_number: "required|phoneNumber",
-            email: "required|email",
-            password: "required|minLength:2",
-            photo_profile: "required|url",
-        })
-
-        schema.check().then((matched) => {
-            if (!matched) {
-                res.status(422).send({
-                    status: false,
-                    message: schema.errors,
-                    data: null
-                })
-                return;
-            }
-        })
-
-        // Check Unique Email
-        const checkEmail =
-            await usersModel.checkEmail(email);
-
-        if (checkEmail.length > 0) {
-            res.status(400).json({
-                status: false,
-                message: "Email is already registered",
-            });
-
-            return;
-        }
-        const request = await usersModel.addUsers({
-            first_name,
-            last_name,
-            phone_number,
-            email,
-            password,
-            photo_profile,
-        });
-        
-        if (request.length > 0) {
-            res.json({
-                status: true,
-                message: "Insert data success",
-            });
-            return;
-        }
-    } catch (error) {
-        res.status("502").json({
-            status: false,
-            message: "something wrong in our server",
-            data: [],
-        });
-    }
-});
+router.post(
+    "/users/register",
+    usersController._validationAddUsers,
+    usersController._addUsers
+);
 
 // Users login
 
@@ -133,25 +55,23 @@ router.post("/users/login", async (req, res) => {
             await database`SELECT * FROM users WHERE email = ${email}`;
 
         if (checkEmail.length == 0) {
-            const schema = new Validator (req.body, {
+            const schema = new Validator(req.body, {
                 email: "required|email",
-                password: "required|minLength:2"
-            })
-    
+                password: "required|minLength:2",
+            });
+
             schema.check().then((matched) => {
                 if (!matched) {
                     res.status(422).send({
                         status: false,
                         message: schema.errors,
-                        data: null
-                    })
+                        data: null,
+                    });
                     return;
                 }
-            })
+            });
             return;
         }
-
-        
 
         // Check if password correct
         const isMatch = bcrypt.compareSync(password, checkEmail[0].password);
@@ -194,7 +114,7 @@ router.get("/users/me", checkJwt, async (req, res) => {
             data: request,
         });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         res.status("502").json({
             status: false,
             message: "something wrong in our server",
@@ -223,18 +143,18 @@ router.put("/users/edit", checkJwt, async (req, res) => {
             phone_number: "required|phoneNumber",
             email: "required|email",
             photo_profile: "required|url",
-        })
+        });
 
         schema.check().then((matched) => {
             if (!matched) {
                 res.status(422).send({
                     status: false,
                     message: schema.errors,
-                    data: null
-                })
+                    data: null,
+                });
                 return;
             }
-        })
+        });
 
         const request = await database`UPDATE users SET ${database(
             req.body,
@@ -274,18 +194,18 @@ router.put("/users/edit/password", checkJwt, async (req, res) => {
 
         const schema = new Validator(req.body, {
             password: "required|minLength:2",
-        })
+        });
 
         schema.check().then((matched) => {
             if (!matched) {
                 res.status(422).send({
                     status: false,
                     message: schema.errors,
-                    data: null
-                })
+                    data: null,
+                });
                 return;
             }
-        })
+        });
 
         res.status("200").json({
             status: true,
@@ -301,6 +221,5 @@ router.put("/users/edit/password", checkJwt, async (req, res) => {
         });
     }
 });
-
 
 module.exports = router;
